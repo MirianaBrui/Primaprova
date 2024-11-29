0453f56b878ef64459fde482f6bd1bbd68df1dcc
Miriana Brui <mirianabrui7@gmail.com>
2024-11-29 17:14:51 +0100

    primo commit

---
+++ TrovaDuplicato/src/NumeroDuplicati/duplicati.java
@@ -0,0 +1,29 @@
+package NumeroDuplicati;
+
+public class duplicati {
+
+	public static void main(String[] args) {
+		 // Dichiarazione dell'array di numeri interi
+        int[] array = {4, 3, 2, 7, 8, 2, 3, 1, 8};
+
+        System.out.print("Elementi duplicati: ");
+        
+
+        
+        for (int i = 0; i < array.length; i++) { //utilizzo il ciclo for per confrontare ogni elemento dell'arrey con gli altri
+           
+            for (int j = i + 1; j < array.length; j++) { //scorro ogni elemento dell'array
+                if (array[i] == array[j]) { //controllo se gli elementi sono uguali
+                    System.out.print(array[i] + " ");
+                    
+                    break; // stampo il numero duplicato una sola volta
+                }
+            }
+        }
+
+      
+        }
+		
+	}
+
+
